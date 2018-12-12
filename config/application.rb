ce555030316255fb893a87f6b0ff1c82a11721c9
keke <ryehkk@yahoo.co.jp>
2018-12-12 15:12:05 +0900

    add translations for num, activerecord, time

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Carbid
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
     config.generators do |g|
       g.template_engine :slim
       g.test_framework :rspec,
