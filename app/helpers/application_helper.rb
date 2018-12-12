88d797686e3eb86e7d5e0ab7f16c897bb0e72fce
keke <ryehkk@yahoo.co.jp>
2018-12-12 14:24:27 +0900

    add flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 module ApplicationHelper
+  def flash_message(message, klass)
+    content_tag(:div, class: "alert alert-#{klass} alert-dismissible fade show") do
+      concat content_tag(:a, '×', class: 'close', data: { dismiss: 'alert' })
+      concat raw(message)
+    end
+  end
+
   def active?(path)
     "active" if current_page?(path)
   end
+
 end
