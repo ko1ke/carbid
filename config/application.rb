1b2d5638e26b4c2579b8c3a2271f8a9f8a55129e
keke <ryehkk@yahoo.co.jp>
2018-12-12 13:54:14 +0900

    set locale and add some translations

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Carbid
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.i18n.default_locale = :ja
     config.generators do |g|
       g.template_engine :slim
       g.test_framework :rspec,
