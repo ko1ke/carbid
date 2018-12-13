eec7af9bdc4cc563c5fb31784f7eee1754936bd8
keke <ryehkk@yahoo.co.jp>
2018-12-13 21:50:33 +0900

    add scaffold controller for bit
    They do not work yet, need to change actions and views.
    Delete unnecessary like destroy action, update action, etc ...

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'auctions#index'
   devise_for :users
+  resources :auctions, only: [:index] do
+    resources :bids, only: [:index, :new, :create]
+  end
+
   namespace :my do
     resources :auctions
   end
